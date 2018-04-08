96cbfe37e60d7e3399095a157ef6d3c75c8ed88a
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-04-08 14:42:32 +0100

    created indecision app

--- src/components/optionModal.js
+++ src/components/optionModal.js
@@ -7,7 +7,7 @@ const OptionModal = (props) => (
     contentLabel = 'Selected Option'
     onRequestClose = {props.handleClearSelected}
     ariaHideApp={false}
-    closeTimeoutMS = {4000}
+    closeTimeoutMS = {1000}
     className = 'modal'
     >
      <h3 className = 'model__title'> Selected Option </h3>
